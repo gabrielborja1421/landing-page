4fc0af1a695d582b18c7446cd43cabe440b9f114
Gabriel-Borja <gabriel_borja1421@hotmail.com>
2023-07-30 15:20:33 -0600

    proyecto terminado

--- backend/src/models/inventarioDAO.js
+++ backend/src/models/inventarioDAO.js
@@ -28,9 +28,59 @@ module.exports = {
     },
 
     searchByNombre: (nombre, callback) => {
+        // Verificar si el nombre está vacío o contiene solo espacios en blanco
+        if (!nombre.trim()) {
+            console.log('Búsqueda inválida. Por favor, ingresa un nombre válido.');
+            return callback(null);
+        }
+
         let sql = 'SELECT * FROM productos WHERE nombre LIKE ?';
         db.query(sql, `%${nombre}%`, (err, datas) => {
             if (err) throw err;
+
+            if (datas.length === 0) {
+                console.log('No se encontraron resultados.');
+                return callback(null);
+            }
+
+            callback({ data: datas });
+        });
+    },
+    searchById: (id, callback) => {
+        // Verificar si el ID es un número válido
+        if (!Number.isInteger(id) || id <= 0) {
+            console.log('ID inválido. Por favor, ingresa un ID numérico válido.');
+            return callback(null);
+        }
+
+        let sql = 'SELECT * FROM productos WHERE id = ?';
+        db.query(sql, id, (err, datas) => {
+            if (err) throw err;
+
+            if (datas.length === 0) {
+                console.log('No se encontraron resultados con el ID proporcionado.');
+                return callback(null);
+            }
+
+            callback(datas[0]);
+        });
+    },
+    getByCategoria: (categoria, callback) => {
+        // Verificar si la categoría está vacía o contiene solo espacios en blanco
+        if (!categoria.trim()) {
+            console.log('Búsqueda inválida. Por favor, ingresa una categoría válida.');
+            return callback(null);
+        }
+
+        let sql = 'SELECT * FROM productos WHERE categoria = ?';
+        db.query(sql, categoria, (err, datas) => {
+            if (err) throw err;
+
+            if (datas.length === 0) {
+                console.log('No se encontraron resultados.');
+                return callback(null);
+            }
+
             callback({ data: datas });
         });
     }
--- backend/src/routers/inventarioRoutes.js
+++ backend/src/routers/inventarioRoutes.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const inventarioDAO = require('../models/inventarioDAO');
 
+// Nueva ruta para buscar productos por categoría
+router.get('/buscarPorCategoria', (req, res) => {
+   const categoria = req.query.categoria;
+   inventarioDAO.getByCategoria(categoria, (data) => {
+    res.send(data);
+   });
+});
+
 // Obtener todos los productos
 router.get('/', (req, res) => {
    inventarioDAO.getAllInventario((data) => {
@@ -13,6 +21,13 @@ router.get('/', (req, res) => {
 router.get('/buscar', (req, res) => {
    const nombre = req.query.q;
    inventarioDAO.searchByNombre(nombre, (data) => {
+    res.send(data);
+   });
+});
+// Nueva ruta para buscar productos por ID
+router.get('/:id', (req, res) => {
+   const id = parseInt(req.params.id);
+   inventarioDAO.searchById(id, (data) => {
     res.send(data);
    });
 });
